eadeb8ca51b666ce60a296e31f95ed9adfa45e79
Fhewn <76724264+Fhewn@users.noreply.github.com>
2022-02-23 20:25:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
+let show = document.querySelector("p");
+let show1 = document.querySelector("h2");
 
+let basla = document.querySelector(".start");
+let durdur = document.querySelector(".stop");
+let reset = document.querySelector(".restart");
+
+let i = 0;
+let deneme;
+let saatg=0;
+let dakika=0;
+let saniye=0;
+function saat1() {
+  deneme = setInterval(saat, 1000);
+
+  function saat() {
+    i++;
+    saniye = i;
+    if (saniye >59){
+        i = 0;
+        saniye = 0;
+        dakika ++; 
+        if (dakika>59){
+          dakika=0;
+          saatg++;
+        }
+    }
+    
+    show.textContent = saatg + " : " + dakika + " : " + saniye;
+  }
+}
+
+basla.onclick = function() {
+  saat1();
+};
+
+durdur.onclick = function() {
+  clearInterval(deneme);
+};
+
+reset.onclick = function (){
+clearInterval(deneme);
+saatg=0;
+dakika=0;
+saniye=0;
+i=0;
+show.textContent = saatg + " : " + dakika + " : " + saniye;
+};
